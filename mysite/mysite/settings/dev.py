7873e98bfa2a1582dd3dd072869dc26b83296dd9
HelderMenegatti <heldermenegatti720@gmail.com>
2021-06-26 23:44:13 -0300

    Instalação dos primeiros componentes para o blog

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3)7z)j3*34*u4rn0kl7-p(b6xf%r-!1*hy3k0dz1)sry)mgd%5'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
