efc33dc1cf56072389edf8775ebc273893085500
Helder <heldermenegatti720@gmail.com>
2021-06-28 17:01:46 -0300

    Alterações nos tamplates e modelos da home

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -8,6 +8,13 @@ from wagtail.admin.edit_handlers import FieldPanel
 class HomePage(Page):
     body = RichTextField(blank=True)
 
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ]
